d7d977c23756ab59be000fb86dea964437b0a2d0
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2023-04-08 19:38:19 -0700

    Update workflows
    Add pre-commit, black, and fix flake8 and isort.
    Remove qa workflow

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
-import plotly.express as px
-import pandas as pd
 import json
-from dash import Dash, dcc, html, Input, Output
+
+import pandas as pd
+import plotly.express as px
+from dash import Dash, Input, Output, dcc, html
 
 app = Dash(__name__)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+colors = {"background": "#111111", "text": "#7FDBFF"}
 
 styles = {
-    'pre': {
-        'border': 'thin lightgrey solid',
-        'overflowX': 'scroll',
-        'width': '48%',
-        'margin-right': '2%'
+    "pre": {
+        "border": "thin lightgrey solid",
+        "overflowX": "scroll",
+        "width": "48%",
+        "margin-right": "2%",
     }
 }
 
 
 def display_choropleth():
-    df = pd.read_csv("./data/county_voter_reg_stats.csv",
-                     dtype={"fips": str})
+    df = pd.read_csv("./data/county_voter_reg_stats.csv", dtype={"fips": str})
 
     counties = json.load(open("./data/counties.geojson"))
-    fig = px.choropleth(df, geojson=counties, color="Party",
-                        locations="County", featureidkey="properties.name")
+    fig = px.choropleth(
+        df,
+        geojson=counties,
+        color="Party",
+        locations="County",
+        featureidkey="properties.name",
+    )
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
@@ -34,22 +36,37 @@ def display_choropleth():
 
     return fig
 
-app.layout = html.Div([
-    html.H4('State of California Election Data'),
-    dcc.Graph(id="graph", figure=display_choropleth(), config={}, style={'width': '48%', 'float': 'left', 'margin-right': '2%'}),
-    html.Div([
-        dcc.Markdown("""
+
+app.layout = html.Div(
+    [
+        html.H4("State of California Election Data"),
+        dcc.Graph(
+            id="graph",
+            figure=display_choropleth(),
+            config={},
+            style={"width": "48%", "float": "left", "margin-right": "2%"},
+        ),
+        html.Div(
+            [
+                dcc.Markdown(
+                    """
                 **Hover Data**
 
                 Mouse over values in the graph.
-            """),
-        html.Pre(id='hover-data', style=styles['pre'])
-    ], className='three columns'),
-])
+            """
+                ),
+                html.Pre(id="hover-data", style=styles["pre"]),
+            ],
+            className="three columns",
+        ),
+    ]
+)
+
 
 @app.callback(
-    Output('hover-data', 'children'),
-    [Input('graph', 'clickData')],)
+    Output("hover-data", "children"),
+    [Input("graph", "clickData")],
+)
 def display_hover_data(hoverData):
     data = {}
     header = [
@@ -66,7 +83,8 @@ def display_hover_data(hoverData):
         "Other",
         "No Party Preference",
         "FIPS",
-        "Party"]
+        "Party",
+    ]
 
     jsondata = json.load(open("data/county_voter_reg_stats.json"))
 
@@ -75,13 +93,16 @@ def display_hover_data(hoverData):
             break
         if header[i] == "County":
             continue
-        county = hoverData['points'][0]['location']
+        county = hoverData["points"][0]["location"]
         data[header[i]] = jsondata[county][header[i]]
 
-    returnvalue = json.dumps(data, indent=2) if data != {} else "Click on a county to see data"
+    returnvalue = (
+        json.dumps(data, indent=2)
+        if data != {}
+        else "Click on a county to see data"
+    )
 
     return returnvalue
 
 
 app.run_server(debug=True)
-
